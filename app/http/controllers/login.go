933faaff3e663d10b6d471de9e6f6cc93bccf588
Toby Yan <me@tobyan.com>
2019-02-19 12:23:12 +0800

    add login support
    Signed-off-by: Toby Yan <me@tobyan.com>

--- app/http/controllers/login.go
+++ app/http/controllers/login.go
@@ -2,10 +2,25 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/totoval/framework/http/controller"
+	"github.com/totoval/framework/model"
+	"net/http"
+	"totoval/app/http/requests"
+	"totoval/app/models"
 )
 
-type Login struct{}
+type Login struct{
+	controller.BaseController
+}
 
-func (*Login) Login(c *gin.Context) {
+func (l *Login) Login(c *gin.Context) {
+	// validate and assign requestData
+	var requestData requests.UserLogin
+	if !l.Validate(c, &requestData) {return}
 
+	user := models.User{}
+	model.First(&user)
+
+	c.JSON(http.StatusOK, gin.H{"data": user})
+	return
 }
