1955d8125ba0f5f29e0306917066c0c1fdc093ab
Toby Yan <me@tobyan.com>
2019-02-19 12:23:23 +0800

    update register controller
    Signed-off-by: Toby Yan <me@tobyan.com>

--- app/http/controllers/register.go
+++ app/http/controllers/register.go
@@ -2,27 +2,34 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/totoval/framework/http/controller"
 	"github.com/totoval/framework/model"
 	"net/http"
 	"totoval/app/http/requests"
 	"totoval/app/models"
 )
 
-type Register struct{}
+type Register struct{
+	controller.BaseController
+}
 
-func (*Register) Register(c *gin.Context) {
+func (r *Register) Register(c *gin.Context) {
+	// validate and assign requestData
 	var requestData requests.UserRegister
-	if err := c.ShouldBindJSON(&requestData); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
+    if !r.Validate(c, &requestData) {return}
 
-	user := models.User{}
+	user := models.User{
+		Email: requestData.Email,
+		Password: requestData.Password,
+	}
 	/**
 	requests.UserRegister struct must contains models.User struct field
 	*/
-	if err := model.Create(requestData, &user); err != nil {
+	if err := model.Create(&user); err != nil {
 		c.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
 		return
 	}
+
+	c.JSON(http.StatusOK, gin.H{"data": user})
+	return
 }
