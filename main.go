e4893904eedaf5b5dc3e59fa7acf1fcdf0c4890f
Toby Yan <me@tobyan.com>
2019-03-05 19:04:59 +0800

    upgrade gin validator v8 - v9
    Signed-off-by: Toby Yan <me@tobyan.com>

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 	c "github.com/totoval/framework/config"
 	"github.com/totoval/framework/model"
+	"gopkg.in/go-playground/validator.v9"
+	"reflect"
+	"sync"
 	"totoval/config"
 	"totoval/routes"
 )
@@ -15,8 +19,9 @@ func init() {
 
 func main() {
 
-	//@cautions do not write DB ops at here
-
+	// upgrade gin validator v8 to v9
+	binding.Validator = new(defaultValidator)
+	
 	r := gin.Default()
 
 	r.Use(gin.Logger())
@@ -26,4 +31,54 @@ func main() {
 	routes.Register(r)
 
 	r.Run(":" + c.GetString("app.port"))
+}
+
+
+
+
+// gin validator v8 to v9
+type defaultValidator struct {
+	once     sync.Once
+	validate *validator.Validate
+}
+
+var _ binding.StructValidator = &defaultValidator{}
+
+func (v *defaultValidator) ValidateStruct(obj interface{}) error {
+
+	if kindOfData(obj) == reflect.Struct {
+
+		v.lazyinit()
+
+		if err := v.validate.Struct(obj); err != nil {
+			return error(err)
+		}
+	}
+
+	return nil
+}
+
+func (v *defaultValidator) Engine() interface{} {
+	v.lazyinit()
+	return v.validate
+}
+
+func (v *defaultValidator) lazyinit() {
+	v.once.Do(func() {
+		v.validate = validator.New()
+		v.validate.SetTagName("binding")
+
+		// add any custom validations etc. here
+	})
+}
+
+func kindOfData(data interface{}) reflect.Kind {
+
+	value := reflect.ValueOf(data)
+	valueType := value.Kind()
+
+	if valueType == reflect.Ptr {
+		valueType = value.Elem().Kind()
+	}
+	return valueType
 }
