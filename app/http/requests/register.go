6e65ad63620c7f74d65fc1be76c924cb5a987ae0
Toby Yan <me@tobyan.com>
2019-04-17 14:09:18 +0800

    omitempty affiliation from code in registration
    Signed-off-by: Toby Yan <me@tobyan.com>

--- app/http/requests/register.go
+++ app/http/requests/register.go
@@ -2,7 +2,7 @@ package requests
 
 type UserRegister struct {
     Email                string  `json:"email" binding:"required,email"`
-    AffiliationFromCode  *string `json:"affiliation_code" binding:"len=6"`
+    AffiliationFromCode  *string `json:"affiliation_code" binding:"omitempty,len=6"`
     Password             string  `json:"password" binding:"required,min=8,max=24"`
     PasswordConfirmation string  `json:"password_confirmation" binding:"required,eqfield=Password"`
 }
