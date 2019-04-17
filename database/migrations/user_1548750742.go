440a32a9633c57e253e9f23dc71cf920637055fb
Toby Yan <me@tobyan.com>
2019-04-17 16:15:39 +0800

    update migration for table name support
    Signed-off-by: Toby Yan <me@tobyan.com>

--- database/migrations/user_1548750742.go
+++ database/migrations/user_1548750742.go
@@ -1,38 +1,45 @@
 package migrations
 
 import (
-	"github.com/jinzhu/gorm"
-	"github.com/totoval/framework/database/migration"
-	"time"
+    "github.com/jinzhu/gorm"
+    "github.com/totoval/framework/database/migration"
+    "github.com/totoval/framework/model"
+
+    "time"
 )
 
 func init() {
-	migration.AddMigrator(&User1548750742{})
+    migration.AddMigrator(&User1548750742{})
 }
 
 type User struct {
-	ID    uint   `gorm:"column:user_id;primary_key;auto_increment"`
-	Name  string `gorm:"column:user_name;type:varchar(100)"`
-	Email string `gorm:"column:user_email;type:varchar(100);unique_index"`
-	//Telephone  string     `gorm:"column:user_telephone;type:varchar(100);unique_index"`
-	Password string `gorm:"column:user_password;type:varchar(100)"`
-	//VerifiedAt time.Time  `gorm:"column:user_verified_at"`
-	CreatedAt *time.Time `gorm:"column:user_created_at"`
-	UpdatedAt time.Time  `gorm:"column:user_updated_at"`
-	DeletedAt *time.Time `gorm:"column:user_deleted_at"`
+    ID    uint   `gorm:"column:user_id;primary_key;auto_increment"`
+    Name  string `gorm:"column:user_name;type:varchar(100)"`
+    Email string `gorm:"column:user_email;type:varchar(100);unique_index"`
+    //Telephone  string     `gorm:"column:user_telephone;type:varchar(100);unique_index"`
+    Password string `gorm:"column:user_password;type:varchar(100)"`
+    //VerifiedAt time.Time  `gorm:"column:user_verified_at"`
+    CreatedAt *time.Time `gorm:"column:user_created_at"`
+    UpdatedAt time.Time  `gorm:"column:user_updated_at"`
+    DeletedAt *time.Time `gorm:"column:user_deleted_at"`
+    model.BaseModel
+}
+
+func (u *User) TableName() string {
+    u.SetTableName("user")
 }
 
 type User1548750742 struct {
-	migration.MigratorIdentify
-	migration.MigrationUtils
+    migration.MigratorIdentify
+    migration.MigrationUtils
 }
 
 func (*User1548750742) Up(db *gorm.DB) *gorm.DB {
-	db = db.CreateTable(&User{})
-	return db
+    db = db.CreateTable(&User{})
+    return db
 }
 
 func (*User1548750742) Down(db *gorm.DB) *gorm.DB {
-	db = db.DropTableIfExists(&User{})
-	return db
+    db = db.DropTableIfExists(&User{})
+    return db
 }
