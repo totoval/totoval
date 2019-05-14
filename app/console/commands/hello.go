5df30b6cd71e2962cd94bd86d8525788c40f3e49
Toby Yan <me@tobyan.com>
2019-05-14 14:49:22 +0800

    add customer commandline support
    Signed-off-by: Toby Yan <me@tobyan.com>

--- app/console/commands/hello.go
+++ app/console/commands/hello.go
@@ -1,7 +1,35 @@
 package commands
 
-import "fmt"
+import (
+	"fmt"
 
-func main() {
+	"github.com/urfave/cli"
+
+	"github.com/totoval/framework/cmd"
+)
+
+func init() {
+	cmd.Add(&HelloWorld{})
+}
+
+type HelloWorld struct {
+}
+
+func (hw *HelloWorld) Command() string {
+	return "say:hello-world"
+}
+
+func (hw *HelloWorld) Aliases() []string {
+	return []string{
+		"c",
+	}
+}
+
+func (hw *HelloWorld) Description() string {
+	return "Say Hello"
+}
+
+func (hw *HelloWorld) Handler(c *cli.Context) error {
 	fmt.Println("Hello World")
+	return nil
 }
