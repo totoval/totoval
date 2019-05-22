fa8fd4042e2eb5c0bad6c96f0d5df92f1c40a11c
Toby Yan <me@tobyan.com>
2019-05-22 16:48:26 +0800

    add graceful shutdown feature
    Signed-off-by: Toby Yan <me@tobyan.com>

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
+	"os/signal"
 	"reflect"
 	"sync"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
@@ -12,11 +17,14 @@ import (
 	"github.com/totoval/framework/cache"
 	c "github.com/totoval/framework/config"
 	"github.com/totoval/framework/database"
+	"github.com/totoval/framework/graceful"
+	"github.com/totoval/framework/helpers/log"
 	"github.com/totoval/framework/helpers/m"
 	"github.com/totoval/framework/helpers/zone"
 	"github.com/totoval/framework/http/middleware"
 	"github.com/totoval/framework/logs"
 	"github.com/totoval/framework/queue"
+
 	"totoval/app/events"
 	"totoval/app/jobs"
 	"totoval/app/listeners"
@@ -80,9 +88,31 @@ func main() {
 
 	//go hub.On("add-user-affiliation")  // go run artisan.go queue:listen add-user-affiliation
 
-	if err := s.ListenAndServe(); err != nil {
-		panic(err)
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Fatal(err.Error())
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	// kill (no param) default send syscanll.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall. SIGKILL but can"t be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Info("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer graceful.ShutDown(ctx)
+	defer cancel()
+
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown: ", logs.Field{"error": err})
 	}
+
+	log.Info("Server exiting")
 }
 
 // gin validator v8 to v9
