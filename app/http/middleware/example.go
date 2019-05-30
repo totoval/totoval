b19c239ac1e30dcad422c83eaeb9d3634c7e52e7
Toby Yan <me@tobyan.com>
2019-05-30 11:23:10 +0800

    fix the middleware example bug
    Signed-off-by: Toby Yan <me@tobyan.com>

--- app/http/middleware/example.go
+++ app/http/middleware/example.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"github.com/totoval/framework/helpers/log"
+	"github.com/totoval/framework/logs"
 
 	"github.com/gin-gonic/gin"
 
@@ -21,10 +22,10 @@ func Example() gin.HandlerFunc {
 
 		// after request
 		latency := zone.Since(t)
-		log.Print(latency)
+		log.Info("latency", logs.Field{"latency": latency})
 
 		// access the status we are sending
 		status := c.Writer.Status()
-		log.Info(status)
+		log.Info("status", logs.Field{"status": status})
 	}
 }
