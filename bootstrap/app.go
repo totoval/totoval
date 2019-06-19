a4af5a0894211c6163c1e97ed626667ebff01cfd
Toby Yan <me@tobyan.com>
2019-06-19 09:51:07 +0800

    change lang pack initialize index
    Signed-off-by: Toby Yan <me@tobyan.com>

--- bootstrap/app.go
+++ bootstrap/app.go
@@ -8,6 +8,7 @@ import (
 	"github.com/totoval/framework/logs"
 	"github.com/totoval/framework/queue"
 	"github.com/totoval/framework/utils/validator"
+
 	"totoval/app/events"
 	"totoval/app/jobs"
 	"totoval/app/listeners"
@@ -19,10 +20,10 @@ func Initialize() {
 	config.Initialize()
 	logs.Initialize()
 	zone.Initialize()
+	lang.Initialize() // an translation must contains resources/lang/xx.json file (then a resources/lang/validation_translator/xx.go)
 	cache.Initialize()
 	database.Initialize()
 	m.Initialize()
-	lang.Initialize() // an translation must contains resources/lang/xx.json file (then a resources/lang/validation_translator/xx.go)
 	queue.Initialize()
 	jobs.Initialize()
 	events.Initialize()
